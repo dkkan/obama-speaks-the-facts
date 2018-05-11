ccc3878f40318f9ddd0f7dc90969ba356e8cc812
David Kan <dkkan@uw.edu>
2018-05-10 20:59:25 -0700

    Added request POST parameters

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 
@@ -14,12 +14,25 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """
+    Executes POST request
+    :url: http://talkobamato.me/synthesize.py
+    :data: input_text=This+is+a+small+change%2C+but+a+big+move+for+us.+140+was+an+arbitrary+choice+based+on+the+160+
+    character+SMS+limit.+Proud+of+how+thoughtful+the+team+has+been+in+solving+a+real+problem+people+have+when+trying+
+    to+tweet.+And+at+the+same+time+maintaining+our+brevity%2C+speed%2C+and+essence%21
+    :return:
+    """
+
+    r = requests.post(url="http://talkobamato.me/synthesize.py",
+                      data={'input_text': get_fact()},
+                      allow_redirects=False)
+
+    return r.headers['Location']
 
 
 if __name__ == "__main__":
